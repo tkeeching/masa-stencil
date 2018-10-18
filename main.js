9faa4c03e67e5f4d836ff54ca41c87737b498c0f
Jun Yeu Mah <junyeumah@gmail.com>
2018-10-18 22:09:27 +1100

    Added highlights and changed fonts

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var lines = [
-    "IT'S k A j QUARTER",
-    "TEN HALF TWENTY",
+    "IT'S k a j quarter",
+    "ten half TWENTY",
     "FIVE l MINUTES p",
-    "TO PAST v ELEVEN",
-    "NINE FOUR EIGHT",
-    "TWO FIVE TWELVE",
-    "SEVEN SIX THREE",
-    "ONE TEN r O'CLOCK"
+    "to PAST v eleven",
+    "nine four eight",
+    "two five TWELVE",
+    "seven six three",
+    "one ten r o'clock"
 ];
 
 $(".face").append("<div class='background'><div class='letters'></div></div>");
@@ -15,10 +15,19 @@ lines.forEach(function(line, lineNo) {
     $(".letters").append("<div class='line line-" + lineNo + "'></div>");
 
     line.split("").forEach(function(char) {
+        console.log(char);
+        console.log(char.toUpperCase());
+
         if (char === "'") {
             $(".line-" + lineNo + " p:last-of-type").append("<span class='tick'>" + char.toUpperCase() + "</span>");
         }
-        else if (char !== " ") {
+        else if (char === " ") {
+
+        }
+        else if (char.toUpperCase() === char) {
+            $(".line-" + lineNo).append("<p class='char highlight'>" + char.toUpperCase() + "</p>");
+        }
+        else if (char.toLowerCase() === char) {
             $(".line-" + lineNo).append("<p class='char'>" + char.toUpperCase() + "</p>");
         }
     })
